22d1e86609d343301357ccbc5409c9fcf1625479
Aythami Déniz Morales <aythami-92@hotmail.com>
2021-12-23 20:15:47 +0000

    Añadimos a los environments nuestra configuración de firebase

--- Rural_Core/src/environments/environment.ts
+++ Rural_Core/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAaoEFb2pS_5C_MECqHPTDKUwExH7VqrT4",
+    authDomain: "rural-core-b4cf7.firebaseapp.com",
+    projectId: "rural-core-b4cf7",
+    storageBucket: "rural-core-b4cf7.appspot.com",
+    messagingSenderId: "460378670468",
+    appId: "1:460378670468:web:44cd6360ad2e5d5f4409d1",
+    measurementId: "G-3G84E8JHKS"
+  }
 };
 
 /*
